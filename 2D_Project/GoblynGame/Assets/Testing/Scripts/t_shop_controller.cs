674435744624400cdffb14ef3c79cd25a24b0f4e
WASDMagician <WASDMagician@gmail.com>
2016-08-22 10:46:06 +0100

    dialogue_system - shop - very basic saving

---
+++ 2D_Project/GoblynGame/Assets/Testing/Scripts/t_shop_controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class t_shop_controller : MonoBehaviour {
+
+	public weapon_object weapon_one;
+	public weapon_object weapon_two;
+	public weapon_object weapon_three;
+
+	public t_item_controller item_one;
+	public t_item_controller item_two;
+	public t_item_controller item_three;
+
+	void Start(){
+		Set_Item_Details (item_one, weapon_one);
+		Set_Item_Details (item_two, weapon_two);
+		Set_Item_Details (item_three, weapon_three);
+	}
+
+	public void Set_Weapons(weapon_object _weapon_one, weapon_object _weapon_two, weapon_object _weapon_three){
+		weapon_one = _weapon_one;
+		weapon_two = _weapon_two;
+		weapon_three = _weapon_three;
+
+		Set_Item_Details (item_one, weapon_one);
+		Set_Item_Details (item_two, weapon_two);
+		Set_Item_Details (item_three, weapon_three);
+	}
+
+	void Set_Item_Details(t_item_controller _item, weapon_object _object){
+		_item.Set_Details (_object.sprite_image, _object.name, _object.damage, _object.cost);
+	}
+}
---
+++ 2D_Project/GoblynGame/Assets/Testing/Scripts/weapon_object.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "base weapon", menuName = "Custom Objects/Weapon", order=1)]
+public class weapon_object : ScriptableObject {
+	public enum weapon_types {melee, ranged};
+	public weapon_types type;
+
+	public GameObject arrow_prefab;
+
+	public Sprite sprite_image;
+	public string name;
+	public int damage;
+	public int cost;
+	public int range;
+
+	public float attack_ignore_animation_start;
+	public float attack_animation_damage_duraction;
+	public float attack_ignore_animation_end;
+}
